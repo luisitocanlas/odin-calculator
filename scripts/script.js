7608fb61ea15474c20fa289648895f335dd0f1ae
Luisito Canlas <luisitocanlas@gmail.com>
2024-04-10 16:24:03 -0700

    Created basic math functions

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+// Variables
+const num1 = 5;
+const num2 = 3;
+
+// Calculation function
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
