e74ab7c3792aab859547afbc1abfe861f55ddbaf
Luisito Canlas <luisitocanlas@gmail.com>
2024-04-10 17:40:15 -0700

    Updated variable

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,15 @@
-// Variables
-const num1 = 5;
-const num2 = 3;
+// Constants
 
-// Functions
+// querySelectors
+let output = document.querySelector('#output');
+
+// Event Listeners
+
+// Calculator function
 function operate(operation, num1, num2) {
 	return operation(num1, num2);
 }
 
-// Calculator function
 const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
