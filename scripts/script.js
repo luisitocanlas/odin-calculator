8b53b21598803ccdd8e11072725219a355353030
Luisito Canlas <luisitocanlas@gmail.com>
2024-04-11 13:31:08 -0700

    Optimized calculator function

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,4 @@
 // Constants
-const BLACK = '#000000';
-const BLUE = '#14213d';
-const ORANGE = '#fca311';
-const PLATINUM = '#e5e5e5';
-const WHITE = '#ffffff';
-
 const output = document.querySelector('#output');
 const numBtns = document.querySelectorAll('#num-container button');
 const opBtns = document.querySelectorAll('#operation-container button');
@@ -20,12 +14,7 @@ let op = '';
 // Event Listeners
 numBtns.forEach((button) => {
 	button.addEventListener('click', () => {
-		if (
-			output.textContent === '+' ||
-			output.textContent === '-' ||
-			output.textContent === '*' ||
-			output.textContent === '/'
-		) {
+		if (['+', '-', '*', '/'].includes(output.textContent)) {
 			output.textContent = '';
 		}
 
@@ -42,43 +31,25 @@ opBtns.forEach((button) => {
 
 		switch (btnId) {
 			case 'clear':
+				num1 = '';
+				num2 = '';
+				op = '';
 				output.textContent = '0';
 				break;
 
 			case 'divide':
-				num1 = +output.textContent;
-				op = 'divide';
-				output.textContent = '/';
-				break;
-
 			case 'multiply':
-				num1 = +output.textContent;
-				op = 'multiply';
-				output.textContent = '*';
-				break;
-
 			case 'subtract':
-				num1 = +output.textContent;
-				op = 'subtract';
-				output.textContent = '-';
-				break;
-
 			case 'add':
 				num1 = +output.textContent;
-				op = 'add';
-				output.textContent = '+';
+				op = btnId;
+				output.textContent = button.textContent;
 				break;
 
 			case 'equals':
-				if (
-					op === 'add' ||
-					op === 'subtract' ||
-					op === 'multiply' ||
-					op === 'divide'
-				) {
+				if (op && num2 !== '') {
 					num2 = +output.textContent;
-					const result = operate(op, num1, num2);
-					output.textContent = result.toString();
+					output.textContent = operate(op, num1, num2);
 					op = '';
 				}
 				break;
@@ -87,26 +58,18 @@ opBtns.forEach((button) => {
 });
 
 // Calculator function
-function operate(operation, num1, num2) {
+function operate(operation, a, b) {
 	switch (operation) {
 		case 'add':
-			return add(num1, num2);
+			return (a + b).toFixed(2);
 
 		case 'subtract':
-			return subtract(num1, num2);
+			return (a - b).toFixed(2);
 
 		case 'multiply':
-			return multiply(num1, num2);
+			return (a * b).toFixed(2);
 
 		case 'divide':
-			return divide(num1, num2);
+			return b !== 0 ? (a / b).toFixed(2) : 'Error';
 	}
 }
-
-const add = (a, b) => (a + b).toFixed(2);
-
-const subtract = (a, b) => (a - b).toFixed(2);
-
-const multiply = (a, b) => (a * b).toFixed(2);
-
-const divide = (a, b) => (a / b).toFixed(2);
